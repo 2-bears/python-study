e49f5b5d260a095eedf5f8e9cff196783fb6b739
2-bears <mouhua.hu@yahoo.com>
2019-02-15 18:01:13 +0800

    add some examples

--- python2/print_plus.py
+++ python2/print_plus.py
@@ -1,8 +1,8 @@
 #coding:utf-8
 import sys
 
-#��ӡ�ӷ���
-#ѧϰpython������ѭ������ʽ�����
+#打印加法表
+#学习python变量，循环，格式化输出
 for m in range(1, 10):
    for n in range (1, m+1):
      print ("%d+%d=%d " %(m, n, m+n)),
